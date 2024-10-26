a096fc24db686de0ef2bed5bac5d07d7231e0e00
Inzamul Hoque <30847431+inzamrzn918@users.noreply.github.com>
2024-10-26 14:20:25 +0530

    Added docs and models (#1)
    * 1. Added core app.
    2. Added models
    
    * 1. Added docs

--- owner/models.py
+++ owner/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from base_user.models import BaseUser
+from core.models import Vehicle, TimeStampMixin
+
+
+class Owner(TimeStampMixin):
+    company_name = models.CharField(max_length=512, null=True)
+    extra_information = models.JSONField()
+
+
+
+class OwnerUsers(TimeStampMixin):
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    user = models.ForeignKey(BaseUser, on_delete=models.CASCADE)
+
+
+class OwnerVehicle(TimeStampMixin):
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
