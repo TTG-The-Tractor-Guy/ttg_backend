a5aedbc84a938fef5edd4fa662851aff9caae7c8
Inzamul Hoque <inzamol@gmail.com>
2024-10-26 10:14:04 +0530

    Removed username field

--- ttg_backend/settings.py
+++ ttg_backend/settings.py
@@ -81,10 +81,28 @@ WSGI_APPLICATION = 'ttg_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'ttg',
+
+        'USER': 'postgres',
+
+        'PASSWORD': '3304',
+
+        'HOST': '127.0.0.1',
+
+        'PORT': 5432,
+
     }
 }
 
